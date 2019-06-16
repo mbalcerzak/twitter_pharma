50a72525125a8ac7a8c68a23ea729879797d7c6c
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2019-06-16 18:00:16 +0200

    tweet_text_get.py update
    - improved code readability
    - added information pronted about scraping progress
    - changed few variables names (comapny_names -> companies)

--- web_scraping/tweet_text_get.py
+++ web_scraping/tweet_text_get.py
@@ -1,68 +1,58 @@
-import simplejson as json
 import tweepy
 import csv
-import pandas as pd     # To handle data
-import numpy as np      # For number computing
 import os
-import unicodedata
 
-# For plotting and visualization:
-from IPython.display import display
-import matplotlib.pyplot as plt
-import seaborn as sns
+path = os.path.join(os.getcwd(), 'tweet_texts_pharma/')
 
-#Twitter API credentials
-consumer_key = "8bLIFLRBX1mMvicwJYl7Uh57k"
-consumer_secret = "cxjaFxSCGv7awlbAVDQnmaItITI9TNxTvnFDdD7qbntsd9lj5q"
-access_key = "1098576703939661824-6pf9AYwb6JzsG8WIKfNU0VdaOLZ629"
-access_secret = "z16DSEjQXEZwLKK3D0wWo7SbbGiIUGsGqDCqKu6wdaS4S"
+# Twitter API credentials in a separate txt file
+keys = {}
 
-outtweets = []
-
+for line in open(path + 'twitter_keys.txt', 'r').readlines():
+    key, var = line.split("=")
+    keys[key.strip()] = var.replace('\n','').strip()
+    
+# authorize twitter, initialize tweepy
+auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+auth.set_access_token(keys['access_key'], keys['access_secret'])
+api = tweepy.API(auth, wait_on_rate_limit=True, 
+                 wait_on_rate_limit_notify = True)
 
 def get_all_tweets(screen_name):
-    #Twitter only allows access to a users most recent 3240 tweets with this method
-    
-    #authorize twitter, initialize tweepy
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify = True)
+    outtweets = []
 
-    a = 0
-
-    for tweet_info in tweepy.Cursor(api.user_timeline, id='AstraZeneca', tweet_mode='extended').items():
+    for tweet_info in tweepy.Cursor(api.user_timeline, id = screen_name, 
+                                    tweet_mode='extended').items():
+        # adding a way to add retweeted texts
         if 'retweeted_status' in dir(tweet_info):
-            tweet='RT ' + tweet_info.retweeted_status.full_text
+            tweet= tweet_info.retweeted_status.full_text
             likes = tweet_info.retweeted_status.favorite_count
+            retweet = True
         else:
-            tweet=tweet_info.full_text
+            tweet = tweet_info.full_text
             likes = tweet_info.favorite_count
-        
-        #print(tweet)
-        a += 1
-        if a%1000==0: print(a)      
+            retweet = False   
 
-        outtweets.append([tweet_info.id_str, tweet_info.created_at, tweet, tweet_info.source, 
-                  likes, tweet_info.retweet_count, tweet_info.entities.get('hashtags')])
+        outtweets.append([tweet_info.id_str, tweet_info.created_at, tweet, 
+                          retweet, tweet_info.source, likes, 
+                          tweet_info.retweet_count, 
+                          tweet_info.entities.get('hashtags')])
     
-
     #write the csv  
-    with open('%s_tweets.txt' % screen_name, 'w',  encoding="utf8") as f:
+    with open(path + '%s_tweets.txt' % screen_name, 'w', encoding="utf8") as f:
         writer = csv.writer(f, delimiter = '|')
-        writer.writerow(["id","created_at","text", "source", "fav", "RT", "hashtags"])
+        writer.writerow(["id","created_at","text", "retweet", "source", "fav", 
+                         "RT", "hashtags"])
         writer.writerows(outtweets)
-    
-    # pass
+        
+    print('Done for ' + screen_name + '. Tweets: ' + str(len(outtweets)))
+    print(str(companies.index(company) + 1) + '/' + str(len(companies)))
 
+# Pharmaceutical companies twitter usernames
+companies = ["AstraZeneca", "JNJCares", "Roche", "Pfizer","Novartis",
+                 "BayerPharma","Merck","GSK","Sanofi","abbvie", "AbbottNews",
+                 "AbbottGlobal","LillyPad","Amgen","bmsnews","GileadSciences"]
 
-#if __name__ == '__main__':
-	#pass in the username of the account you want to download
-
-
-company_names = ["AstraZeneca", "JNJCares", "Roche", "Pfizer","Novartis","BayerPharma","Merck","GSK","Sanofi","abbvie",
-                "AbbottNews","AbbottGlobal","LillyPad","Amgen","bmsnews","GileadSciences"]
-
-for company in company_names:
+for company in companies:
 	get_all_tweets(company)
 
 
